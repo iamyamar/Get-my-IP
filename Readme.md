7cc8874c82a3939735142dfd2c902558b3704cb0
Yash <kumar.yash9313@gmail.com>
2020-05-27 01:23:46 +0530

    created

---
+++ Readme.md
@@ -0,0 +1,20 @@
+-------------  GET IP -- website  ----------------               
+
+---------------   What to add --------------------
+1)IP address     ---|
+                    |----API (Json)(myip.com)       https://app.ipgeolocation.io/
+2)Country        ---|
+
+3)Internet Service Provider 
+4)Country
+5)Browser
+6)OS
+7)Google map Location
+
+---------------    Technology Used -----------------
+1)Html
+2)Css - Sass,Bootstrap
+3)JavaScript - Jquery,Ajax
+4)npm for modules
+5)Git for Version Control
+
---
+++ script/script.js
@@ -0,0 +1,91 @@
+ google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Global Share',
+                       'width':300,
+                       'height':200,
+                       'is3D': true,};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+      google.charts.setOnLoadCallback(myChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function myChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'My Share',
+                       'width':300,
+                       'height':200,
+                        'pieHole': 0.3,
+                        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_piv'));
+        chart.draw(data, options);
+      }
+       google.charts.setOnLoadCallback(allChart);
+    function allChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ["Copper", 8.94, "#b87333"],
+        ["Silver", 10.49, "silver"],
+        ["Gold", 19.30, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Density of Precious Metals, in g/cm^3",
+        width: 300,
+        height: 200,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("chart_hiv"));
+      chart.draw(view, options);
+  }
